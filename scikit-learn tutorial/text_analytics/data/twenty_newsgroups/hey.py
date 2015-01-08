690e812c29116f95433cc10898ae25c1df677531
luming <lh00000000@gmail.com>
2015-01-08 10:30:31 -0500

    moved python code

--- scikit-learn tutorial/text_analytics/data/twenty_newsgroups/hey.py
+++
@@ -1,4 +0,0 @@
-categories = ['alt.atheism', 'soc.religion.christian', 'comp.graphics', 'sci.med']
-
-from sklearn.datasets import fetch_20newsgroups
-twenty_train = fetch_20newsgroups(subset='train', categories=categories, shuffle=True, random_state=42)
